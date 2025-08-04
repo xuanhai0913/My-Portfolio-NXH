5fb4007928cf9057ab34d90b979b47df567c8bd1
Nguyễn Xuân Hải <73636134+xuanhai0913@users.noreply.github.com>
2025-08-04 14:16:44 +0700

    Simplify Discord verification with rewrites and headers

--- src/app/api/.well-known/discord/route.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
-  res.status(200).send('dh=af38fe5712a5e3ccba62d6b68166ca8779de8c40');
-}
