c3887ced6cf48619b17764066580300092e7f015
xuanhai0913 <xuanhai0913750452@gmail.com>
2026-01-27 00:22:31 +0700

    fix: add missing useState import in SectionTransition

--- src/components/SectionTransition/index.jsx
+++ src/components/SectionTransition/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import './SectionTransition.css';
